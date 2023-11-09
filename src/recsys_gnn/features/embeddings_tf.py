8f4107df2146fa56b331f24bb39b80df63820eb6
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-11-09 20:35:50 +0300

    Add BoW embeddings function

--- src/recsys_gnn/features/embeddings_tf.py
+++ src/recsys_gnn/features/embeddings_tf.py
@@ -10,6 +10,41 @@ logger = logging.getLogger(__name__)
 __all__ = ["generate_tf_embeddings"]
 
 
+def get_tf_embeddings_BOW(
+    df: pd.DataFrame,
+    feature: str,
+    max_features: int,
+    max_len: int,
+    col_name: str,
+):
+    """
+    Generates embeddings based on Bag-of-Words approach.
+
+    feature: List of strings with fixed sequence of items e.g. ['123 24 15 0 0 0', '54 35 0 0 0 0']
+    max_features: Maximum vocab size.
+    max_len: Sequence length to pad the outputs to.
+    col_name: Name of columns in output DataFrame with embeddings/
+    """
+    text_dataset = tf.data.Dataset.from_tensor_slices(df[feature].tolist())
+    vectorize_layer = tf.keras.layers.TextVectorization(
+        max_tokens=max_features,
+        output_mode="int",
+        output_sequence_length=max_len,
+    )
+    vectorize_layer.adapt(text_dataset.batch(64))
+
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.Input(shape=(1,), dtype=tf.string))
+    model.add(vectorize_layer)
+
+    emb = model.predict(df[feature])
+    emb = pd.DataFrame(
+        emb, index=df.index, columns=[f"{col_name}_{i}" for i in range(max_len)]
+    )
+
+    return emb, model
+
+
 def fit_transform_embeddings(
     df: pd.DataFrame,
     feature: str,
